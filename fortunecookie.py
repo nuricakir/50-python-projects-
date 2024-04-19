3934686b7af383e1748c4d8c765f7f32f851f21b
Nuri Çakır <112883476+nuricakir@users.noreply.github.com>
2024-04-19 20:50:49 +0300

    Update fortunecookie.py

--- fortunecookie.py
+++ fortunecookie.py
@@ -11,41 +11,41 @@ print("=========================================================================
 
 if cookie == 1:
     print("Your Lucky Number 1!\nCookie Message)""Every good and perfect gift is from above.")
-if cookie == 2:
+elif cookie == 2:
     print("Your Lucky Number 2!\nCookie Message)""The most effective way to do it, is to do it.")
-if cookie == 3:
+elif cookie == 3:
     print("Your Lucky Number 3!\nCookie Message)""It does not do to dwell on dreams and forget to live.")
-if cookie == 4:
+elif cookie == 4:
     print("Your Lucky Number 4!\nCookie Message)""Do it scared..")
-if cookie == 5:
+elif cookie == 5:
     print("Your Lucky Number 5!\nCookie Message)""If you want the rainbow, you gotta put up with the rain!")
-if cookie == 6:
+elif cookie == 6:
     print("Your Lucky Number 6!\nCookie Message)""Love all, trust a few, do wrong to none.")
-if cookie == 7:
+elif cookie == 7:
     print("Your Lucky Number 7!\nCookie Message)""Live as if you were to die tomorrow. Learn as if you were to live forever.")
-if cookie == 8:
+elif cookie == 8:
     print("Your Lucky Number 8!\nCookie Message)""No matter what people tell you, words and ideas can change the world.")
-if cookie == 9:
+elif cookie == 9:
     print("Your Lucky Number 9!\nCookie Message)""Only an aunt can give hugs likea mother, can keep secrets like a sister and share love like a friend.")
-if cookie == 10:
+elif cookie == 10:
     print("Your Lucky Number 10!\nCookie Message)""When someone leaves, its because someone else is about to arrive.")
-if cookie == 11:
+elif cookie == 11:
     print("Your Lucky Number 11!\nCookie Message)""Be yourself, everyone else is already taken.")
-if cookie == 12:
+elif cookie == 12:
     print("Your Lucky Number 12!\nCookie Message)""Real love stories never have endings.")
-if cookie == 13:
+elif cookie == 13:
     print("Your Lucky Number 13!\nCookie Message)""With you, I am home.")
-if cookie == 14:
+elif cookie == 14:
     print("Your Lucky Number 14!\nCookie Message)""You make it look easy. The fact is it's really hard and you do it anyway.")
-if cookie == 15:
+elif cookie == 15:
     print("Your Lucky Number 15!\nCookie Message)""You can't know, you can only believe. Or not.")
-if cookie == 16:
+elif cookie == 16:
     print("Your Lucky Number 16!\nCookie Message)""Some things have to be believed to be seen.")
-if cookie == 17:
+elif cookie == 17:
     print("Your Lucky Number 17!\nCookie Message)""In life there is no real safety except for self-belief.")
-if cookie == 18:
+elif cookie == 18:
     print("Your Lucky Number 18!\nCookie Message)""If not me, who? If not now, when?")
-if cookie == 19:
+elif cookie == 19:
     print("Your Lucky Number 19!\nCookie Message)""We loved with a love that was more than love.")
-if cookie == 20:
+elif cookie == 20:
     print("Your Lucky Number 20!\nCookie Message)""All that you are is all that I'll ever need.")
